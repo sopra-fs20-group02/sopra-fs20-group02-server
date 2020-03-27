5c0e91c799807155ed2c97e0b617fe3b9d66ebdf
Andrin Rehmann <andrinrehmann@gmail.com>
2020-03-27 02:41:31 -0700

    progress on tests, some things are not clear about implemented rest interface

--- src/test/java/ch/uzh/ifi/seal/soprafs20/rest/InterfaceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/rest/InterfaceTest.java
@@ -1,18 +1,31 @@
 package ch.uzh.ifi.seal.soprafs20.rest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import ch.uzh.ifi.seal.soprafs20.exceptions.SopraServiceException;
+import ch.uzh.ifi.seal.soprafs20.rest.dto.UserPostDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.net.http.HttpHeaders;
+
 import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,14 +34,53 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class InterfaceTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    /**
+     * Default test
+     * @throws Exception
+     */
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")));
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("The application is running.");
+    }
+
+    /**
+     * User registration with valid information
+     * @throws Exception
+     */
+    @Test
+    public void userRegistration_givenValidInformation_shouldReturnUser() throws Exception {
+        UserPostDTO userPostDTO = new UserPostDTO();
+        userPostDTO.setName("Test User");
+        userPostDTO.setUsername("testUsername");
+
+        assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/users",
+                asJsonString(userPostDTO),
+                user)).contains("The application is running.");
+    }
+
+    /**
+     * Helper Method to convert userPostDTO into a JSON string such that the input can be processed
+     * Input will look like this: {"name": "Test User", "username": "testUsername"}
+     * @param object
+     * @return string
+     */
+    private String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        }
+        catch (JsonProcessingException e) {
+            throw new SopraServiceException(String.format("The request body could not be created.%s", e.toString()));
+        }
     }
 }
