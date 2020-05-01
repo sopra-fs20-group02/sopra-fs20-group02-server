610f055d365db732b9f0f962447fe111de117995
Andrin Rehmann <andrinrehmann@gmail.com>
2020-05-01 09:35:57 -0700

    removed unnecessary status entry

--- src/main/java/ch/uzh/ifi/seal/soprafs20/constant/GameStatus.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/constant/GameStatus.java
@@ -1,5 +1,5 @@
 package ch.uzh.ifi.seal.soprafs20.constant;
 
 public enum GameStatus {
-    WAITING, FULL, FINISHED, WON, DRAW, WHITE_IN_CHECK, BLACK_IN_CHECK
+    WAITING, FULL, WON, DRAW, WHITE_IN_CHECK, BLACK_IN_CHECK
 }
