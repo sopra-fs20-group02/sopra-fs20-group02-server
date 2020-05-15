8909dfe1e0550652f2baf51383b1ebbcb560d8b7
Andrin Rehmann <andrinrehmann@gmail.com>
2020-05-15 15:17:01 -0700

    changes

--- src/main/java/ch/uzh/ifi/seal/soprafs20/websocket/model/ChatMessage.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/websocket/model/ChatMessage.java
@@ -5,25 +5,30 @@ public class ChatMessage {
     private String content;
     private String sender;
 
-    public enum MessageType {
+    public enum MessageType
+    {
         CHAT,
         JOIN,
         LEAVE
     }
 
-    public MessageType getType() {
+    public MessageType getType()
+    {
         return type;
     }
 
-    public void setType(MessageType type) {
+    public void setType(MessageType type)
+    {
         this.type = type;
     }
 
-    public String getContent() {
+    public String getContent()
+    {
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent(String content)
+    {
         this.content = content;
     }
 
